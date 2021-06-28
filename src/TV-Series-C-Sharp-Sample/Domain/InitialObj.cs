9405efa925068457d92f1f7a5cdd9821092a4f5c
Daniel Lim-Apo <daniel.limapo@gmail.com>
2021-06-28 12:40:40 -0300

    Starting IRepository interface

---
+++ src/TV-Series-C-Sharp-Sample/Domain/InitialObj.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace TV_Series_C_Sharp_Sample
+{  
+    public abstract class InitialObj {
+        public abstract int Id {get; set;}
+    }
+}
---
+++ src/TV-Series-C-Sharp-Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TV_Series_C_Sharp_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
